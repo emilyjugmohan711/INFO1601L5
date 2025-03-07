144ed7dd05cc27419b2fc0a64bfc2113bd8b0c11
emilyjugmohan148 <emilyjugmohan148@gmail.com>
2025-03-06 23:01:59 -0400

    Update

--- index.js
+++ index.js
@@ -1,5 +1,95 @@
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];// 10 elements
+function getAverageGrade(student, course) {
 
-for(let i=0; i< arr.length; i++){
-  console.log(arr[i])
+  let sum = 0;
+
+  for (let record of student.transcript) {
+      if (record.course === course) {
+        for (let grade of record.grades) {
+          sum += grade;
+        }
+        return sum / 3;
+      }
+  }
+
+  return -1;
 }
+
+function getAssignmentMark(student, course, num) {
+
+  for (let record of student.transcript) {
+      if (record.course === course) {
+          if (num >= 1 && num <= 3) {
+              return record.grades[num - 1]; 
+          }
+      }
+  }
+
+  return -1;
+}
+
+function averageAssessment(students, courseName, assignment) {
+
+  let sum = 0;
+  let count = 0;
+
+  for (let student of students) {
+      let mark = getAssignmentMark(student, courseName, assignment);
+      if (mark !== -1) {
+          sum += mark;
+          count++;
+      }
+  }
+
+  if (count === 0) {
+      return -1;  
+  }
+
+  return sum / count;
+}
+
+//object literal
+let bob = {
+  fname: "bob",
+  lname: "smith",
+  age: 18,
+  height: 6,
+  transcript:[
+    {
+      course: 'INFO 1603',
+      grades: [ 89, 34, 67 ]
+    },
+    {
+      course: 'INFO 1601',
+      grades: [ 89, 34, 67 ]
+    }
+  ]
+};
+
+let sally = {
+  fname: "sally",
+  lname: "smith",
+  age: 18,
+  height: 6,
+  transcript:[
+    {
+      course: 'INFO 1601',
+      grades: [ 100, 89, 79 ]
+    }
+  ]
+};
+
+let paul = {
+  fname: "paul",
+  lname: "smith",
+  age: 18,
+  height: 6,
+  transcript:[
+    {
+      course: 'INFO 1600',
+      grades: [ 89, 34, 67 ]
+    }
+  ]
+};
+
+
+const students = [bob, sally, paul];
